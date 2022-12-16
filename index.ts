811530e38429e639d3dfd224120c79725a167edc
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-17 01:53:51 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,21 +1,17 @@
-import {
-  fromEvent,
-  exhaustMap,
-  tap,
-  of,
-  delay,
-  startWith,
-} from 'rxjs';
+import { fromEvent, tap, of, delay, startWith } from 'rxjs';
+import { exhaustMap, mergeMap } from 'rxjs';
 
 const clicks = fromEvent(document.getElementById('save'), 'click');
-const mockSaveDocument = of('saved !').pipe(
-  delay(10000),
-  startWith('saving...')
+const mockSaveDocument = of('done !').pipe(
+  delay(5000),
+  startWith('saving doc...')
 );
 
 const saveDocument = clicks.pipe(
   tap((click) => console.log('New Save Request...')),
-  exhaustMap(() => mockSaveDocument)
+  exhaustMap(() => mockSaveDocument),
+  //Compare exhaustMap by replacing with mergeMap
+  //mergeMap(() => mockSaveDocument)
 );
 
 saveDocument.subscribe((x) => console.log(x));
