4d016d37f2b4d4d6aada7e3815b4c33a0334c318
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2023-01-17 20:15:11 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -4,13 +4,14 @@ import { exhaustMap, mergeMap } from 'rxjs';
 const clicks = fromEvent(document.getElementById('save'), 'click');
 const mockSaveDocument = of('done !').pipe(
   delay(5000),
-  startWith('Called saving doc...')
+  startWith('Called save doc...')
 );
 
 const saveDocument = clicks.pipe(
   tap((click) => console.log('New Save Request...')),
   exhaustMap(() => mockSaveDocument)
-  //Compare exhaustMap by replacing with mergeMap
+  //Compare exhaustMap by replacing with mergeMap,
+  //every request should make a call to save.
   //mergeMap(() => mockSaveDocument)
 );
 
